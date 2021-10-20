66c7cb02d8289060b06f2c95a0bb6b07d58038b2
Jessica Guo <jessicaguo@email.arizona.edu>
2021-10-20 13:45:05 -0700

    Change shape to represent wood anatomy

--- scripts/plot/03-Js.R
+++ scripts/plot/03-Js.R
@@ -6,21 +6,26 @@ library(ggthemes)
 
 # Read in data
 load("clean-data/sapflow/Gc_daily.Rdata") # d
-load("clean-data/sapflow/Js_daily_sum.Rdata") # Jm_sum
+load("clean-data/sapflow/Js_daily_sum.Rdata") # Js_sum
 
 # Summarize
 Js_sum %>%
   group_by(species) %>%
   summarize(min_Js = min(Js_mean),
             max_Js = max(Js_mean),
-            mean_Js = mean(Js_mean))
+            mean_Js = mean(Js_mean)) 
+
+# Add wood anatomy
+Js_sum <- Js_sum %>%
+  mutate(anatomy = ifelse(species %in% c("T. ramosissima", "E. angustifolia"),
+                          "ring", "diffuse"))
 
 fig_Js <- ggplot(Js_sum, aes(x = as.Date(date), col = site)) + 
   geom_errorbar(aes(ymin = Js_mean - Js_sd, 
                     ymax = Js_mean + Js_sd),
                 alpha = 0.3,
                 width = 0) +
-  geom_point(aes(y = Js_mean)) +
+  geom_point(aes(y = Js_mean, shape = anatomy)) +
   facet_wrap(~species, scales = "free_y") +
   scale_y_continuous(expression(paste(J[s]," (g ", m^-2, " ", s^-1, ")"))) +
   scale_x_date(limits = range(as.Date(d$date)), 
@@ -31,6 +36,7 @@ fig_Js <- ggplot(Js_sum, aes(x = as.Date(date), col = site)) +
                                "Reservoir", 
                                "Parley's",
                                "Upper")) +
+  scale_shape_manual(values = c(17, 19)) +
   facet_wrap(~species, ncol = 3, scales = "free_y") +
   theme_bw(base_size = 12) +
   theme(panel.grid.minor = element_blank(),
@@ -39,7 +45,9 @@ fig_Js <- ggplot(Js_sum, aes(x = as.Date(date), col = site)) +
         strip.text = element_text(face = "italic"),
         axis.title.x = element_blank(),
         legend.title = element_blank()) +
-  guides(color = guide_legend(override.aes = list(linetype = 0)))
+  guides(color = guide_legend(override.aes = list(linetype = 0,
+                                                  shape = 17)),
+         shape = "none")
 
 jpeg(filename = "plots/Fig3_Js.jpg", width = 7, height = 4, 
      units = "in", res = 600)
